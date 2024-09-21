268558be273a868e4d6e6c0d4cf83a73bcea7c19
McCormick95 <mccormick.ryan.95@gmail.com>
2024-09-21 15:24:51 -0400

    Added functionality for decode()

--- LAB_C_02/Encoder.c
+++ LAB_C_02/Encoder.c
@@ -7,6 +7,8 @@
 
 
 #define MAX_LENGTH 256
+#define OUTPUT_FILE "encoded_out.txt"
+#define INPUT_FILE "unencoded.txt"
 
 typedef struct {
     char character;
@@ -21,23 +23,20 @@ char *read_string();
 void write_string(char *encoded_string);
 
 int main(){
-
-    CharValue charValues[] = {
-        {'A',10}, {'a',10}, {'B',11}, {'b',11}, {'C',12}, {'c',12}, {'D',13}, {'d',13}, {'E',14}, {'e',14},
-        {'F',15}, {'f',15}, {'G',16}, {'g',16}, {'H',17}, {'h',17}, {'I',18}, {'i',18}, {'J',19}, {'j',19}, 
-        {'K',20}, {'k',20}, {'L',21}, {'l',21}, {'M',22}, {'m',22}, {'N',23}, {'n',23}, {'O',24}, {'o',24},
-        {'P',25}, {'p',25}, {'Q',26}, {'q',26}, {'R',27}, {'r',27}, {'S',28}, {'s',28}, {'T',29}, {'t',29}, 
-        {'U',30}, {'u',30}, {'V',31}, {'v',31}, {'W',32}, {'w',32}, {'X',33}, {'x',33}, {'Y',34}, {'y',34}, 
-        {'Z',35}, {'z',35}, {' ',36}, {'.',37}, {'?',38}, {'!',39}
-    };
-
-    // Calculate the size of the array
-    int arraySize = sizeof(charValues) / sizeof(charValues[0]);
-    //printCharValues(charValues, arraySize);
-
     char user_selection;
     char user_selection_lower;
-    
+
+    CharValue charValues[] = {
+        {'a',10}, {'b',11}, {'c',12}, {'d',13}, {'e',14},
+        {'f',15}, {'g',16}, {'h',17}, {'i',18}, {'j',19}, 
+        {'k',20}, {'l',21}, {'m',22}, {'n',23}, {'o',24},
+        {'p',25}, {'q',26}, {'r',27}, {'s',28}, {'t',29}, 
+        {'u',30}, {'v',31}, {'w',32}, {'x',33}, {'y',34}, 
+        {'z',35}, {' ',36}, {'.',37}, {'?',38}, {'!',39}
+    };
+
+    int arraySize = sizeof(charValues) / sizeof(charValues[0]);
+
     printf("Welcome to the Encoder / Decoder program! \n");
     printf(" Please select d for decode or e for encode: e \n");
     scanf(" %c", &user_selection);
@@ -53,8 +52,6 @@ int main(){
     int choice_e = strcmp(selection_str, "e");
     int choice_d = strcmp(selection_str, "d");
 
-    
-
     if(choice_e == 0){
         encode(charValues, arraySize);
     }
@@ -69,13 +66,7 @@ int main(){
 }
 
 void encode(CharValue* charValues, int size){
-
-    char *file_string_buffer = read_string();
-    
-    //toLowerCase(buffer);
-    //printf("test sent after lower: %s \n", buffer);
-    //int str_length = buffer.Length;
-
+    char *file_string_buffer = read_string(INPUT_FILE);
     char encoded_string[MAX_LENGTH];
     char encode_temp[10];
     int encode_value;
@@ -90,16 +81,47 @@ void encode(CharValue* charValues, int size){
             }
         }
     }
-
     write_string(encoded_string);    
 }
 
 void decode(CharValue* charValues, int size){
+    char decoded_string[MAX_LENGTH];
+    decoded_string[0] = '\0';
+    int decoded_temp;
 
+    char* encoded_string = read_string(OUTPUT_FILE);
+    int string_length = strlen(encoded_string);
+    printf("STRING READ IN: %s\n", encoded_string);
+
+    if(string_length % 2 != 0){
+        printf("ERROR: THE ENCODED STRING IS NOT EVEN");
+        exit(0);
+    }
+
+    for(int i = 0; encoded_string[i] != '\0'; i += 2){
+        char encode_pair[3];
+        encode_pair[0] = encoded_string[i];
+        encode_pair[1] = encoded_string[i + 1];
+        encode_pair[2] = '\0';
+        decoded_temp = atoi(encode_pair);
+        printf("TESTING - DECODED_PAIR: %d \n", decoded_temp);
+
+        for (int j=0; j < size; j++){
+            if(charValues[j].value == decoded_temp){
+                char temp_char[2];
+                temp_char[0] = charValues[j].character;
+                temp_char[1] = '\0';
+                printf("TESTING - TEMP_CHAR: %s \n", temp_char);
+                strcat(decoded_string, temp_char);
+                printf("TESTING - DECODED_STRING: %s \n", decoded_string);
+            }
+        }
+    }
+    printf("DECODED STRING: %s \n", decoded_string);
 }
 
 void toLowerCase(char* str) {
-    for (int i = 0; str[i]; i++) {
+    for (int i = 0; str[i] != '\0'; i++) {
         str[i] = tolower(str[i]);
     }
 }
@@ -110,11 +132,11 @@ void printCharValues(CharValue* charValues, int size) {
     }
 }
 
-char *read_string(){
+char *read_string(char *file_name){
     FILE *file;
     static char file_string_buffer[MAX_LENGTH];
 
-    file = fopen("unencoded.txt", "r");
+    file = fopen(file_name, "r");
     if(file == NULL){
         perror("ERROR WHILE OPENING FILE");
         exit(0);
@@ -134,7 +156,7 @@ char *read_string(){
 } 
 
 void write_string(char *encoded_string){
-    FILE *file_out = fopen("encoded_out.txt", "w");
+    FILE *file_out = fopen(OUTPUT_FILE, "w");
 
     if(file_out == NULL){
         perror("ERROR: WHILE OPENING FILE");
